60051836d66f427fb69318de17b17e975a3840df
Prasi710 <prasi710@turfhub.com>
2026-02-25 19:50:06 +0000

    auto-commit for 4bec2a0d-4984-4357-beb4-2055c18ac9e9

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import { MapPin, Star, Clock, Calendar, X, Phone, Check } from 'lucide-react';
+import { MapPin, Star, Clock, Calendar, X, Phone, Check, User } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
